fead56dae1eb6017de890c98b9ed8685f42c2379
IT24102105 <it24102105@my.sliit.lk>
2025-09-24 10:28:48 +0530

    Add files via upload

---
+++ Lab Sheet 08/IT24102105_Lab8.R
@@ -0,0 +1,46 @@
+#setting the directory
+setwd("C:\\Users\\IT24102105\\Desktop\\IT24102105\\Lab 8")
+getwd()
+
+#importing the dataset
+weights_data <- data<-read.table("Exercise - LaptopsWeights.txt", header=TRUE)
+weights <- weights_data$Weight
+
+#Q1
+pop_mean <- mean(weights)
+pop_sd   <- sd(weights)
+
+# Population Mean
+pop_mean
+# Population Standard Deviation
+pop_sd
+
+
+#Q2
+set.seed(123)  
+n_samples <- 25
+n_size    <- 6
+
+sample_means <- numeric(n_samples)
+sample_sds   <- numeric(n_samples)
+
+for(i in 1:n_samples){
+  samp <- sample(weights, n_size, replace = TRUE)
+  sample_means[i] <- mean(samp)
+  sample_sds[i]   <- sd(samp)
+}
+
+# all 25 sample means
+sample_means
+# all 25 sample standard deviations
+sample_sds
+
+
+#Q3
+mean_of_sample_means <- mean(sample_means)
+sd_of_sample_means   <- sd(sample_means)
+
+# Mean of the 25 sample means
+mean_of_sample_means
+# SD of the 25 sample means
+sd_of_sample_means
